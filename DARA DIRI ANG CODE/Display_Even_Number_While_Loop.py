6d4e36d48a1e400070fe4eaf3b4ed96104186818
Kim Joshua T. Lopez <145188219+KJTL290@users.noreply.github.com>
2025-01-07 09:33:53 +0800

    Refactor Display_Even_Number_While_Loop.py for improved readability and consistency

--- DARA DIRI ANG CODE/Display_Even_Number_While_Loop.py
+++ DARA DIRI ANG CODE/Display_Even_Number_While_Loop.py
@@ -6,22 +6,19 @@ root.title("Display Even Number")
 
 def display_input():
     try:
-
         num = int(entry.get())
         x = 1
         result = ""
         while x <= num:
             if x % 2 == 0:
                 result = str(result) + str(x) + "\n"
-                x += 1
+            x += 1
         messagebox.showinfo("Result", f"{result}")
-            
     except ValueError:
         messagebox.showerror("Error", "Please enter a valid whole number")
 
 def clear():
-    entry.delete(0, tk.END) 
-
+    entry.delete(0, tk.END)
 
 label = tk.Label(root, text="Enter a whole number:")
 label.grid(row=0, column=0, padx=10, pady=10)
@@ -32,7 +29,7 @@ entry.grid(row=0, column=1, padx=10, pady=10)
 button = tk.Button(root, text="Compute", command=display_input)
 button.grid(row=1, column=1, padx=10, pady=10)
 
-button_1 = tk.Button(root, text="Clear", command=clear)
-button_1.grid(row=1, column=2, padx=10, pady=10)    
+clear_button = tk.Button(root, text="Clear", command=clear) 
+clear_button.grid(row=1, column=2, padx=10, pady=10)
 
 root.mainloop()
